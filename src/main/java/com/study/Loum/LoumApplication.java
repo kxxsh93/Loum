1528cc50ddf650755a51d2fb8f207e5886f0fffc
kxxsh93 <gusac@naver.com>
2022-12-19 23:24:39 +0900

    Initial commit

---
+++ src/main/java/com/study/Loum/LoumApplication.java
@@ -0,0 +1,13 @@
+package com.study.Loum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/Loum/LoumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Loum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
